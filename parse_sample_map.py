c540195c5b4046ca336762f06a52add36a50750d
Alexander Hsieh <alexanderhsieh7@gmail.com>
2020-05-29 12:32:53 -0400

    simplified argument parsing

--- parse_sample_map.py
+++ parse_sample_map.py
@@ -1,34 +1,19 @@
 '''
 Script to take as input sample_map and sample_id and output the file path corresponding to sample_id
+Usage: python parse_sample_map.py <sample_map> <sample_id>
 '''
 
 import sys
-from optparse import OptionParser
 
 
 ####################################################################################################
 ## handle arguments
 ####################################################################################################
-parser = OptionParser()
-parser.add_option('-m', '--smap', dest='sample_map',help='sample map (picard)')
-parser.add_option('-s', '--sid', dest='sample_id',help='sample id')
-(options, args) = parser.parse_args()
-
-## check all arguments present
-if (options.sample_id == None or options.sample_map == None):
-	print '\n' + '## ERROR: missing arguments' + '\n'
-	parser.print_help()
-	print '\n'
-	sys.exit()
-
-
-sample_id = options.sample_id
-sample_map = options.sample_map
 
 
 ## parse sample_map 
 pathd = {} # {id : path}
-with open(sample_map, 'r') as smapf:
+with open(sys.argv[1], 'r') as smapf:
   for line in smapf:
     tmp = line.strip().split('\t')
     id = tmp[0]
@@ -37,4 +22,4 @@ with open(sample_map, 'r') as smapf:
 
 
 ## return path
-print(pathd[sample_id])
+print(pathd[sys.argv[2]])
